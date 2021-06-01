be3a2019d2887aabdd24365eaa94199f844ba80c
Andrei Yangabishev <waffleboot@gmail.com>
2021-06-01 23:46:16 +0300

    add cluster

--- cli.sh
+++ cli.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run -it --network some-network --rm redis redis-cli -h some-redis
+docker run -it --network some-network --rm redis redis-cli -c -h redis-a
 
+# docker run -it --network some-network --rm redis redis-cli -h some-redis
+
+# docker exec -it some-redis bash
