d80a6e12ee45dd9e0f7b72a215f2ce70ce411486
Andrei Yangabishev <waffleboot@gmail.com>
2021-06-06 00:17:56 +0300

    golang modle

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+// #define REDISMODULE_EXPERIMENTAL_API
+// #include "redismodule.h"
+// #include <stdlib.h>
+// static void
+// filter (RedisModuleCommandFilterCtx *filter) {
+// }
+// static int
+// HelloworldRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+//     RedisModule_ReplyWithLongLong(ctx,rand());
+//     return REDISMODULE_OK;
+// }
+// int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+//     if (RedisModule_Init(ctx,"filter",1,REDISMODULE_APIVER_1) == REDISMODULE_ERR) return REDISMODULE_ERR;
+//     RedisModule_RegisterCommandFilter(ctx, filter, 0);
+//     if (RedisModule_CreateCommand(ctx,"helloworld.rand",
+//         HelloworldRand_RedisCommand, "fast random",
+//         0, 0, 0) == REDISMODULE_ERR)
+//         return REDISMODULE_ERR;
+//     return REDISMODULE_OK;
+// }
+import "C"
+
 import (
 	"context"
 	"fmt"
