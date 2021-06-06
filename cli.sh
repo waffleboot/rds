1992403d438a356075523d635ac326a6e37a9b47
Andrei Yangabishev <waffleboot@gmail.com>
2021-06-06 23:24:51 +0300

    run goclient inside container

--- cli.sh
+++ cli.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-docker run -it --network redis-network --rm redis redis-cli -h sentinel-1 -p 5000
+# docker run -it --network redis-network --rm redis redis-cli -h sentinel-1 -p 5000
 
 # docker run -it --network some-network --rm redis redis-cli -h some-redis
 
-# docker exec -it some-redis bash
+# docker exec -it sentinel-1 redis-cli -p 5000
+
+# docker run -it --net=host --rm redis redis-cli -p 16379
+
+docker exec -it sentinel-1 redis-cli -p 5000
--- main.go
+++ main.go
@@ -3,29 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 
 	_ "net/http/pprof"
 
-	"net/http"
 	_ "net/http"
 )
 
-var ctx = context.Background()
+type MyLogger struct {
+}
+
+func (_ MyLogger) Printf(ctx context.Context, format string, v ...interface{}) {
+}
+
+func test(ctx context.Context, rdb *redis.Client) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println(rdb.Ping(ctx))
+		case <-ctx.Done():
+			return
+		}
+	}
+}
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+
+	// redis.SetLogger(MyLogger{})
 	rdb := redis.NewFailoverClient(&redis.FailoverOptions{
 		MasterName:    "mymaster",
-		SentinelAddrs: []string{"localhost:5001", "localhost:5002", "localhost:5003"},
+		SentinelAddrs: []string{"10.5.0.5:5000", "10.5.0.6:5000", "10.5.0.7:5000"},
 	})
 	defer rdb.Close()
-	// c, cancel := context.WithTimeout(ctx, 3*time.Second)
-	// defer cancel()
-	// fmt.Println(rdb.Ping(c))
-	fmt.Println(rdb.Ping(ctx))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal, 1)
+	defer close(c)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		signal.Stop(c)
+		cancel()
+	}()
+	test(ctx, rdb)
 }
